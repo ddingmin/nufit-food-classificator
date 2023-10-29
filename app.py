a810cbc561f44a4517dd197a46ea9e3558d188d9
ddingmin <ddingminyt@gmail.com>
2023-10-29 16:18:06 +0900

    config: 서버 구동 환경 설정

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ import model.inference as infer
 
 app = Flask(__name__)
 
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif','webp'])
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif', 'webp'])
+
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 # 분류되는 음식 category들을 전부 반환
 @app.route('/food/list', methods=["GET"])
@@ -14,8 +16,9 @@ def get_food_categories():
     categories = infer.get_classes()
     return jsonify(categories)
 
+
 # 음식 이미지를 입력받아 상위 5개의 음식을 반환
-@app.route('/food', methods=["GET"])
+@app.route('/food', methods=["POST"])
 def inference_image():
     if "image" not in request.files:
         return "Image not received", 400
@@ -25,5 +28,6 @@ def inference_image():
     predictions = infer.inference(image_file)
     return jsonify(predictions)
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000, debug=True)
